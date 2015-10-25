18693e88c517a18448c8d4c87ae01a723b707739
paragp <ferrari20024u@gmail.com>
2015-10-25 16:32:28 +0530

    'first'

---
+++ src/org/opengts/CompileTime.java
@@ -0,0 +1,12 @@
+package org.opengts;
+public class CompileTime
+{
+    // 2015/10/19 17:17:43 IST
+    public static final String SERVICE_ACCOUNT_ID   = "opengts";
+    public static final String SERVICE_ACCOUNT_NAME = "Open Source OpenGTS";
+    public static final String SERVICE_ACCOUNT_HASH = "8E8AD5BD55CCBA73A679AC720DC08B8A";
+    public static final String SERVICE_ACCOUNT_KEY  = "";
+    public static final long   COMPILE_TIMESTAMP    = 1445255263L;
+    public static final String COMPILE_DATETIME     = "2015/10/19 17:17:43 IST";
+    public static final long   COMPILE_SEED         = 0x8C9DE886B2BAAEA6L;
+}
